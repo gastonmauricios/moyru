48da8e2639e1d78fe038abbd08cb5b687bf4056a
gaston <gastonmauricios58@gmail.com>
2020-08-28 15:49:02 -0300

    cambio nueva web

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+jQuery('document').ready(function($) {
+	
+	var menuBtn = $('.menu-icon'),
+	menu = $('.nav ul');
+	
+
+	menuBtn.click(function() {
+
+
+if (menu.hasClass('show')) {
+
+menu.removeClass('show');
+
+} else {
+
+	menu.addClass('show');
+
+}
+
+		/* Act on the event */
+		
+		
+	});
+
+	
+});
